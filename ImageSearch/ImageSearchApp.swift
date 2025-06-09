87d33315fffb4737dfb8f23889c6cd9c573d49fa
Marcell Fulop <marcell.fulop@techconsulting.tech>
2025-06-09 09:08:27 -0400

    Initial Commit

---
+++ ImageSearch/ImageSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSearchApp.swift
+//  ImageSearch
+//
+//  Created by Marcell Fulop on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct ImageSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
