26dfb08c98ad6f06ce271b2c6677ccfa86f12685
Marcell Fulop <marcell.fulop@techconsulting.tech>
2025-06-10 08:27:38 -0400

    added and updated files

--- ImageSearchTests/ImageSearchTests.swift
+++ ImageSearchTests/ImageSearchTests.swift
@@ -9,28 +9,15 @@ import XCTest
 @testable import ImageSearch
 
 final class ImageSearchTests: XCTestCase {
-
-    override func setUpWithError() throws {
+    var serviceManager: ServiceAPI!
+    override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        serviceManager = 
     }
 
-    override func tearDownWithError() throws {
+    override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 
 }
