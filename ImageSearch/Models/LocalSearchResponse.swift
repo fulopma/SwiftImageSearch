5029dfe464d2c3aae4d888368b1942c902e1c1f9
Marcell Fulop <marcell.fulop@techconsulting.tech>
2025-06-12 00:10:11 -0400

    Local search optimization

--- ImageSearch/Models/LocalSearchResponse.swift
+++ ImageSearch/Models/LocalSearchResponse.swift
@@ -1,33 +1,9 @@
-//
-//  LocalSearchResponse.swift
-//  ImageSearch
-//
-//  Created by Marcell Fulop on 6/11/25.
-//
 
-//struct LocalModel: Decodable {
-//    let responses: [LocalResponse]
-//}
-
-//struct LocalResponse: Decodable {
-//    let hits: [LocalPixabayImage]
-//}
-//
-//struct LocalPixabayImage: Decodable {
-//    let previewURL: String
-//}
 
 
 struct LocalResponse: Decodable {
     let total: Int
     let totalHits: Int
-    let hits: [PixabayImage] // or whatever struct represents the image items
+    let hits: [PixabayImage] 
     let query: String
 }
-
-//struct ImageResult: Decodable {
-//    let id: Int
-//    let pageURL: String
-//    let previewURL: String
-//    // other properties...
-//}
