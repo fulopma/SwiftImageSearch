f67fa3857fbdc19a9846c75f82ff92a7b6ebe07f
Marcell Fulop <marcell.fulop@techconsulting.tech>
2025-06-12 00:07:46 -0400

    adding localresponses json reading; cache not working

--- ImageSearch/Network Layer/NetworkLayer.swift
+++ ImageSearch/Network Layer/NetworkLayer.swift
@@ -1,7 +1,6 @@
-
+import Collections
 // TODO: Implement Caching
 import Foundation
-import Collections
 
 public enum ServiceError: Error {
     case invalidURL
@@ -17,11 +16,11 @@ public enum HttpMethod: String {
 }
 
 public protocol Request {
-    var baseURL: String {get set}
-    var path: String {get set}
-    var httpMethod: HttpMethod {get set}
-    var params: [String: String] {get set}
-    var header: [String: String] {get set}
+    var baseURL: String { get set }
+    var path: String { get set }
+    var httpMethod: HttpMethod { get set }
+    var params: [String: String] { get set }
+    var header: [String: String] { get set }
 }
 
 extension Request {
@@ -31,7 +30,7 @@ extension Request {
             URLQueryItem(name: $0.key, value: $0.value)
         }
         guard let url = urlComponents?.url else {
-            
+
             return nil
         }
         var urlRequest = URLRequest(url: url)
@@ -40,13 +39,14 @@ extension Request {
         return urlRequest
     }
 }
- protocol ServiceAPI {
+protocol ServiceAPI {
     func readLocalResponse() -> [LocalResponse]
-    func execute<T: Decodable>(request: Request, modelName: T.Type) async throws -> T
+    func execute<T: Decodable>(request: Request, modelName: T.Type) async throws
+        -> T
     func fetchImage(for searchTerm: String, url: String) async throws -> Data
 }
 
-public class ServiceManager: ServiceAPI{
+public class ServiceManager: ServiceAPI {
     // "searchTerm", ["URL": ImageDataForUrl]
     private var cache: Deque<(String, [String: Data])> = []
     private var localResponses: [LocalResponse] = []
@@ -54,12 +54,14 @@ public class ServiceManager: ServiceAPI{
         cache.reserveCapacity(10)
         localResponses = readLocalResponse()
     }
-    
+
     func readLocalResponse() -> [LocalResponse] {
-        guard let url = Bundle.main.url(
-            forResource: "pixabay_resultsQ",
-            withExtension: "json"
-        ) else {
+        guard
+            let url = Bundle.main.url(
+                forResource: "pixabay_resultsQ",
+                withExtension: "json"
+            )
+        else {
             print("File not found")
             fatalError("no point")
         }
@@ -72,45 +74,57 @@ public class ServiceManager: ServiceAPI{
         let jsonDecoder = JSONDecoder()
 
         do {
-            let responses = try jsonDecoder.decode([LocalResponse].self, from: data)
+            let responses = try jsonDecoder.decode(
+                [LocalResponse].self,
+                from: data
+            )
             return responses
-            
+
         } catch {
             print("Error parsing json: \(error)")
             fatalError("No point in continuing")
         }
     }
-    
+
     private func returnLocalResponse(query: String) -> PixabayResponse? {
-        guard let response = localResponses.first (where: { $0.query == query } ) else {
+        guard let response = localResponses.first(where: { $0.query == query })
+        else {
             return nil
         }
         let pr = PixabayResponse(hits: response.hits)
         return pr
     }
 
-    public func execute<T>(request: any Request, modelName: T.Type) async throws -> T where T : Decodable {
-        if let response = returnLocalResponse(query: request.params["q"]?.lowercased() ?? "") {
-            return response as! T 
+    public func execute<T>(request: any Request, modelName: T.Type) async throws
+        -> T where T: Decodable
+    {
+        if let response = returnLocalResponse(
+            query: request.params["q"]?.lowercased() ?? ""
+        ) {
+            return response as! T
         }
-        
+
         guard let urlRequest = request.createRequest() else {
             throw ServiceError.invalidURL
         }
-        let configuration = URLSessionConfiguration.default // Or .ephemeral, or .background
-        configuration.timeoutIntervalForRequest = 10 // Set request timeout to 30 seconds
-       // configuration.timeoutIntervalForResource = 300 // Set resource timeout to 300 seconds (5 minutes)
+        let configuration = URLSessionConfiguration.default  // Or .ephemeral, or .background
+        configuration.timeoutIntervalForRequest = 10  // Set request timeout to 30 seconds
+        // configuration.timeoutIntervalForResource = 300 // Set resource timeout to 300 seconds (5 minutes)
 
-        let session = URLSession(configuration: configuration) // Initialize URLSession with the configuration
+        let session = URLSession(configuration: configuration)  // Initialize URLSession with the configuration
         session.configuration.httpShouldSetCookies = false
-        let (data, _) =  try await session.data(for: urlRequest)
+        let (data, _) = try await session.data(for: urlRequest)
         session.finishTasksAndInvalidate()
         return try JSONDecoder().decode(
-             modelName.self, from: data)
+            modelName.self,
+            from: data
+        )
     }
-    
-    public func fetchImage(for searchTerm: String, url: String) async throws -> Data {
-        let step1 = cache.filter({$0.0 == searchTerm})
+
+    public func fetchImage(for searchTerm: String, url: String) async throws
+        -> Data
+    {
+        let step1 = cache.filter({ $0.0 == searchTerm })
         if step1.isEmpty {
             return try await getImageData(for: searchTerm, with: url)
         }
@@ -119,25 +133,29 @@ public class ServiceManager: ServiceAPI{
         }
         return imageFromUrl
     }
-    
-    private func getImageData(for searchTerm: String, with url: String) async throws -> Data {
+
+    private func getImageData(for searchTerm: String, with url: String)
+        async throws -> Data
+    {
         guard let urlObj = URL(string: url) else {
-            throw NSError(domain: "Invalid URL \(url)", code: 1001, userInfo: nil)
+            throw NSError(
+                domain: "Invalid URL \(url)",
+                code: 1001,
+                userInfo: nil
+            )
         }
         var request = URLRequest(url: urlObj)
-       request.httpMethod = "GET"
-      // request.setValue("image/*", forHTTPHeaderField: "Accept")
-       request.setValue("Chrome", forHTTPHeaderField: "User-Agent") // simulate browser
+        request.httpMethod = "GET"
+        // request.setValue("image/*", forHTTPHeaderField: "Accept")
+        request.setValue("Chrome", forHTTPHeaderField: "User-Agent")  // simulate browser
 
-       let (rawData, _) = try await URLSession.shared.data(for: request)
+        let (rawData, _) = try await URLSession.shared.data(for: request)
         if cache.isEmpty {
-            cache.append( (searchTerm, [url: rawData]) )
-        }
-        else if cache.count == 10 && cache.last?.0 != searchTerm{
+            cache.append((searchTerm, [url: rawData]))
+        } else if cache.count == 10 && cache.last?.0 != searchTerm {
             let _ = cache.popFirst()
-            cache.append( (searchTerm, [url: rawData]) )
-        }
-        else {
+            cache.append((searchTerm, [url: rawData]))
+        } else {
             for (elementIndex, _) in cache.enumerated() {
                 if cache[elementIndex].0 == searchTerm {
                     cache[elementIndex].1[url] = rawData
